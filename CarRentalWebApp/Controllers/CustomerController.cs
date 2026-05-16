3536528948d9e38975efe1ea67411046b4af8c8d
Enes ASLAN <Enes ASLAN@DESKTOP-FTGHQSH>
2026-05-17 02:32:19 +0300

    CustomerController bitti

---
+++ CarRentalWebApp/Controllers/CustomerController.cs
@@ -0,0 +1,101 @@
+﻿using CarRentalWebApp.Data;
+using CarRentalWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalWebApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CarRentalDbContext _context;
+        public CustomerController(CarRentalDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return View(customers);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customer.RegistrationDate = DateTime.Now;
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FindAsync(Id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, Customer customer)
+        {
+            if (customer != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FirstOrDefaultAsync(c => c.Id == Id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirm(int Id)
+        {
+            var customer = await _context.Customers.FindAsync(Id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
